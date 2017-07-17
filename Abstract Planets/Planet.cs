510609882fdd5ed6fda4506084852610b59e8e73
Dennis Kanzira <Dennis Kanzira@LAPTOP-E0G5NNPS>
2017-07-17 12:02:06 -0400

    Created Planet Abstract class

---
+++ Abstract Planets/Planet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name:Dennis Kanzira
+ * Date:July 17 2017
+ * Description: This is the Abstract Planet class
+ * version: 0.1 - Created Abstract Planet Class
+ * 
+*/
+namespace Abstract_Planets
+{
+    public abstract class Planet
+    {
+
+    }
+}

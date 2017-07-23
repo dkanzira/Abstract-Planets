41cb2a264720a06e99aecf9bcc161ba8036c57c0
Dennis Kanzira <Dennis Kanzira@LAPTOP-E0G5NNPS>
2017-07-23 16:04:42 -0400

    Implemented the IHasMoons and IHabitable interfaces

--- Abstract Planets/IHabitable.cs
+++ Abstract Planets/IHabitable.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+/*
+ * Name:Dennis Kanzira
+ * Date:July 23 2017
+ * Description: This is the Abstract Planet class
+ * version: 0.1 - Created the IHabitable inteface
+ * 
+*/
 namespace Abstract_Planets
 {
     public interface IHabitable
     {
+        bool Habitable();
     }
 }
--- Abstract Planets/TerrestrialPlanet.cs
+++ Abstract Planets/TerrestrialPlanet.cs
@@ -6,12 +6,12 @@ using System.Text;
  * Name:Dennis Kanzira
  * Date:July 23 2017
  * Description: This is the Abstract Planet class
- * version: 0.1 - Create the TerrestrialPlanet, a subclass of Planets
+ * version: 0.2 -Implemented the IHasMoons and IHabitable interfaces
  * 
 */
 namespace Abstract_Planets
 {
-    public class TerrestrialPlanet : Planet
+    public class TerrestrialPlanet : Planet,IHasMoons,IHabitable
     {
         //PRIVATE INSTANCE VARIABLES
         private bool _oxygen
@@ -24,15 +24,31 @@ namespace Abstract_Planets
             {
                 this._oxygen = value;
             }
-         }
+        }
 
         //CONSTRUCTORS ---------------------------------------------
+        /// <summary>
+        /// This is the main constructor of the Terrestrial Planet
+        /// its all the parameters of the Planet base class and oxygen (bool)
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="diameter"></param>
+        /// <param name="mass"></param>
+        /// <param name="oxygen"></param>
         public TerrestrialPlanet(string name, double diameter, double mass,bool oxygen)
             :base(name,diameter,mass)
         {
             this._oxygen = oxygen;
         }
         //PUBLIC METHODS
+        public bool HasMoons()
+        {
+            throw new NotImplementedException();
+        }
 
+        public bool Habitable()
+        {
+            throw new NotImplementedException();
+        }
     }
 }

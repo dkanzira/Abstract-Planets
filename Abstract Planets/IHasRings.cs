c9b281a30f18df5bca1235572dc5f4556307e8d4
Dennis Kanzira <Dennis Kanzira@LAPTOP-E0G5NNPS>
2017-07-23 15:11:05 -0400

    Created The IHasRings, an interface class

--- Abstract Planets/IHasRings.cs
+++ Abstract Planets/IHasRings.cs
@@ -2,7 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+/*
+ * Name:Dennis Kanzira
+ * Date:July 23 2017
+ * Description: This is the Abstract Planet class
+ * version: 0.1 - Created The IHasRings, an interface class
+ *
+*/
 namespace Abstract_Planets
 {
     public interface IHasRings
